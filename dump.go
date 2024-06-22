96abb281f0decefef20e25e6202012d944d5ff02
Lasan Nishshanka <19279535+las-nish@users.noreply.github.com>
2024-06-22 12:38:10 +0530

    Update result dump structure, and fix comments

--- dump.go
+++ dump.go
@@ -2,111 +2,114 @@ package apitest
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
+// dumpReadableHeader is the function to dump the readable headers
 func dumpReadableHeader() string {
-	return ""
+	return fmt.Sprintf("Running API testing...\n\n")
 }
 
-func dumpReadableFooter() string {
+// dumpReadableFooter is the function to dump the readable footer
+func dumpReadableFooter(instance *Instance) string {
 	var output string
-	output = fmt.Sprintf("\n%-23s : %d\n", "total test cases", 1)
-	output += fmt.Sprintf("%-23s : %d\n", "total passed test cases", 1)
-	output += fmt.Sprintf("%-23s : %d\n\n", "total failed test cases", 1)
+	output = fmt.Sprintf("%-23s : %d\n", "total test cases", instance.TotalCases)
+	output += fmt.Sprintf("%-23s : %d/%d\n", "total passed test cases", instance.TotalPassedCases, instance.TotalCases)
+	output += fmt.Sprintf("%-23s : %d/%d\n", "total failed test cases", instance.TotalFailedCases, instance.TotalCases)
 	return output
 }
 
-func dumpReadableItem(result TestCases, config Config) string {
+// dumpReadableItem is the function to dump the readable item
+func dumpReadableItem(tc TestCases, config Config, result analyzedResult) string {
 	var output string
-	output = "\t\t{\n"
-	output += "\t\t},\n"
-
-	return output
-}
-
-func dumpJSONHeader() string {
-	var output string
-	output = "{\n"
-	output += "\t\"test_cases\": [\n"
-	return output
-}
-
-func dumpJSONFooter() string {
-	var output string
-	output = "\t],\n"
-	output += fmt.Sprintf("\t\"total_test_cases\": %d,\n", 1)
-	output += fmt.Sprintf("\t\"total_passed_test_cases\": %d,\n", 1)
-	output += fmt.Sprintf("\t\"total_failed_test_cases\": %d,\n", 1)
-	output += "}\n"
-	return output
-}
-
-func dumpJSONItem(result TestCases, config Config) string {
-	var output string
-	// TODO
-
-	return output
-}
-
-func dumpHeader(config Config) string {
-	switch config.Format {
-	case TableFormat:
-		return dumpReadableHeader()
-	case JSONFormat:
-		return dumpJSONHeader()
-	default:
-		return dumpReadableHeader()
+	if result.IsPassed {
+		output = fmt.Sprintf("Test case %-7s with %s performance [ %s ]\n", result.PassedStatus, result.Performance, tc.Case.ID)
+	} else {
+		output = fmt.Sprintf("Test case %-7s [ %s ]\n", result.PassedStatus, tc.Case.ID)
 	}
+
+	output += fmt.Sprintf("  ├─ %-15s : %s\n", "Details", tc.Case.Details)
+	output += fmt.Sprintf("  ├─ %-15s : %s\n", "Endpoint", tc.Case.EndPoint)
+	output += fmt.Sprintf("  ├─ %-15s : %s\n", "Type", result.Type)
+
+	if result.IsPassed && !config.IsNeedResult {
+		output += fmt.Sprintf("  └─ %-15s : %s\n", "Time", tc.Time)
+	}
+
+	if result.IsPassed && config.IsNeedResult {
+		output += fmt.Sprintf("  ├─ %-15s : %s\n", "Time", tc.Time)
+		output += fmt.Sprintf("  └─ %-15s : %s\n", "Result", strings.TrimSuffix(tc.ResultGot, "\n"))
+	}
+
+	if !result.IsPassed {
+		output += fmt.Sprintf("  ├─ %-15s : %s\n", "Time", tc.Time)
+		output += fmt.Sprintf("  ├─ %-15s : %s\n", "Performance", result.Performance)
+		output += fmt.Sprintf("  ├─ %-15s : Expected %d, but got \033[1;31m%d\033[0m (%s)\n", "Status", tc.Case.ExpectedStatus, tc.StatusCodeGot, tc.StatusGot)
+
+		if config.Level == VerboseLevel {
+			output += fmt.Sprintf("  ├─ %-15s : %s (%d, %d)\n", "Protocol", tc.ProtoGot, tc.ProtoMajorGot, tc.ProtoMinorGot)
+			output += fmt.Sprintf("  ├─ %-15s : %d\n", "Content Length", tc.ContentLengthGot)
+		}
+
+		output += fmt.Sprintf("  └─ %-15s : %s\n", "Result", strings.TrimSuffix(tc.ResultGot, "\n"))
+	}
+
+	return output + "\n"
 }
 
-func dumpItems(cases <-chan TestCases, config Config) string {
+// dumpHeader is the function to dump the header
+func dumpHeader() string {
+	return dumpReadableHeader()
+}
+
+// dumpItems is the function to dump the items
+func dumpItems(cases <-chan TestCases, instance *Instance) string {
 	var output = ""
-	for result := range cases {
-		switch config.Format {
-		case TableFormat:
-			output += dumpReadableItem(result, config)
-		case JSONFormat:
-			output += dumpJSONItem(result, config)
-		default:
-			output += dumpReadableItem(result, config)
+	for tc := range cases {
+		if isEmptyNode(tc) {
+			continue
+		}
+
+		result := instance.analyze(tc)
+		output += dumpReadableItem(tc, instance.Config, result)
+	}
+
+	return output
+}
+
+// dumpFooter is the function to dump the footer
+func dumpFooter(instance *Instance) string {
+	return dumpReadableFooter(instance)
+}
+
+// performOutput is the function to perform the output
+func performOutput(output string, instance *Instance) error {
+	fmt.Print(output)
+
+	if instance.IsNeedExit {
+		if instance.TotalFailedCases > 0 {
+			os.Exit(1)
 		}
 	}
-
-	return output
-}
-
-func dumpFooter(config Config) string {
-	switch config.Format {
-	case TableFormat:
-		return dumpReadableFooter()
-	case JSONFormat:
-		return dumpJSONFooter()
-	default:
-		return dumpReadableFooter()
-	}
-}
-
-func performOutput(output string, config Config) error {
-	// TODO
 
 	return nil
 }
 
-// Dump is a method that writes the test cases to the destination based on the configuration.
-// It takes an instance as an argument.
+// Dump is the function to dump the test cases
+// in readable format
+// It will print the test cases to the console
 //
 // Example:
 //
-//	instance := Instance
-//	/* initialize instance .. */
-//	instance.Cases.Dump(instance.Config)
-func (h *TestCases) Dump(config Config) {
+//	instance.Cases.Dump(instance)
+func (h *TestCases) Dump(instance *Instance) {
 	var output string
 	cases := h.iterate()
-	output = dumpHeader(config)
-	output += dumpItems(cases, config)
-	output += dumpFooter(config)
-	err := performOutput(output, config)
+	output = dumpHeader()
+	output += dumpItems(cases, instance)
+	output += dumpFooter(instance)
+	err := performOutput(output, instance)
 	if err != nil {
 		panic(err)
 	}
